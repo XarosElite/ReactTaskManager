f42cad01d51c04f2690533ded9a4d99a78cac795
XarosElite <polingj999@gmail.com>
2023-05-04 11:17:12 -0400

    Restructured Nav bar so I do not need to add its componet to every page I make. Also made a Blank Projects Page

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
+
+import { BrowserRouter as Router, Route, Link, BrowserRouter, Routes } from 'react-router-dom';
+
 import Dashboard from './componets/Dashboard';
+import NavBar from './componets/NavBar';
+import Projects from './componets/Projects';
 
 function App() {
   return (
     <div className="App">
-      <Dashboard/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NavBar />}>
+          <Route index element={<Dashboard />} />
+          <Route path='projects' element={<Projects />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
--- my-app/src/componets/NavBar.tsx
+++ my-app/src/componets/NavBar.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link , Outlet} from 'react-router-dom';
+import Dashboard from './Dashboard';
+import Projects from './Projects';
+
 
 export default function NavBar() {
   return (
+    <>
     <nav>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Tasks</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">Settings</a></li>
-      </ul>
-    </nav>
-  );
-}
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><a href="#">Tasks</a></li>
+          <li><Link to="/projects">Projects</Link></li>
+          <li><a href="#">Settings</a></li>
+        </ul>
+      </nav>
+      <Outlet/>
+    </>
+      
+  )
+};
